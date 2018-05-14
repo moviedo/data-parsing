c42f65124d8a5286d5b436f5df84dc3d497b60ab
Mauro Oviedo <mauro@zipari.com>
2018-05-13 21:21:22 -0400

    Finished parsing logic.

--- script.js
+++ script.js
@@ -5,31 +5,67 @@ const readline = require('readline');
 const url = 'https://2swdepm0wa.execute-api.us-east-1.amazonaws.com/prod/NavaInterview/measures​';
 const schema = [];
 
-const lineReader = readline.createInterface({
+const schemaReader = readline.createInterface({
   input: fs.createReadStream('./schemas/booleanmeasures.csv')
 });
 
-lineReader.on('line', line => {
+schemaReader.on('line', line => {
   const data = line.split(',');
   schema.push({
     // name of that column in the database table
     name: data[0],
     // characters used by the column in the data file
-    width: data[1],
+    width: parseInt(data[1]),
     // SQL data type that should be used to store the value in the database table.
     // Acceptable values are TEXT, INTEGER and BOOLEAN.
     datatype: data[2]
   });
 });
 
-lineReader.on('close', () => {
-  console.log(schema);
+
+schemaReader.on('close', () => {
+  const dataReader = readline.createInterface({
+    input: fs.createReadStream('./data/booleanmeasures.txt')
+  });
+
+  dataReader.on('line', line => {
+    console.log(getParsedData(line));
+
+    // axios.post(url)
+    //   .then(res => {
+    //     console.log(response);
+    //   })
+    //   .catch(err => {
+    //     console.log(`Error: ${err}`);
+    //   });
+  });
 });
 
-// axios.get(url)
-//   .then(res => {
-//     console.log(response);
-//   })
-//   .catch(err => {
-//     console.log(`Error: ${err}`);
-//   });
+
+
+/**
+ * Method used to parse the csv line data into object.
+ *
+ * @param {String} line
+ * @return {Object}
+ */
+function getParsedData(line) {
+  const parsedData = {};
+  let temp = line;
+
+  schema.forEach(val => {
+    if (val.datatype == 'TEXT')
+      parsedData[val.name] = temp.slice(0, val.width).trim();
+
+    else if (val.datatype === 'BOOLEAN')
+      parsedData[val.name] = Boolean(temp.slice(0, val.width));
+
+    else if (val.datatype === 'INTEGER')
+      parsedData[val.name] = parseInt(temp.slice(0, val.width));
+
+    temp = temp.slice(val.width);
+  });
+
+  return parsedData
+}
+
